9248ecff12b5f5182ab64313b7743b47e3816be3
wei.jin <wei.jin@aispeech.com>
2020-05-03 12:55:30 +0800

    add healthz & sync handler

--- pkg/web/route.go
+++ pkg/web/route.go
@@ -4,13 +4,31 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"github.com/gxthrj/ingress-hw/log"
+	"io"
+	"github.com/gxthrj/ingress-hw/conf"
+	"strconv"
 )
 
 var logger = log.GetLogger()
 
 func Route() *httprouter.Router{
 	router := httprouter.New()
+	router.GET("/healthz", Healthz)
+	router.GET("/sync/:namespace/:name/:port", sync)
 	return router
+}
+
+func Healthz(w http.ResponseWriter, req *http.Request, _ httprouter.Params){
+	io.WriteString(w, "ok")
+}
+
+func sync(w http.ResponseWriter, req *http.Request, p httprouter.Params){
+	ns := p.ByName("namespace")
+	svcName := p.ByName("name")
+	port := p.ByName("port")
+	portNum, _ := strconv.Atoi(port)
+	se := &conf.SyncEvent{Namespace: ns, Name: svcName, Port: int32(portNum)}
+	conf.SyncQueue <- se
 }
 
 func Start(){
